04284b51a5eae3ae0528dfac1085105a7f203f81
Guillermo León <gleonoliva@users.noreply.github.com>
2018-09-01 18:23:49 -0500

    Initial implementation of Kuhn Munkres Algorithm

--- q4/KM.java
+++ q4/KM.java
@@ -2,6 +2,24 @@ import java.util.*;
 
 public class KM {
 
+    int[][] matrix;
+    int[][] assign;
+    
+    ArrayList<HashSet<Pair>> possibleSolutions;
+
+    class Pair {
+        int row, col;
+        
+        public Pair(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+        
+        public String toString() {
+            return String.format("[%d => %d]", row, col);
+        } 
+        
+    }
 
     private int lowest(int[] array) {
         int val = Integer.MAX_VALUE;
@@ -11,12 +29,16 @@ public class KM {
         return val;
     }
     
-    private boolean isAssignmentValid(int[][] assignment) {
-        return false;
-    }
-    
-    private void printSolution(int[][] assignment, int[][] matrix) {
+    private boolean isValidSolution(HashSet<Pair> solution) {
+        HashSet<Integer> x = new HashSet<>();
+        HashSet<Integer> y = new HashSet<>();
         
+        for (Pair p : solution) {
+            x.add(p.row);
+            y.add(p.col);
+        }
+        
+        return x.size() == y.size();
     }
     
     private void copyTo(int[] to, int[] from) {
@@ -25,10 +47,80 @@ public class KM {
         for(int i = 0;i < to.length; i++)
             to[i] = from[i];
     }
+    
+    private void findSolutionCore(int row, HashSet<Pair> solution) {
+    
+        if (row == matrix.length) {
+            // We must have a solution here.
+            if (isValidSolution(solution)) {
+                possibleSolutions.add((HashSet<Pair>)solution.clone());
+            }
+            return;
+        }
+    
+        for (int col = 0; col < matrix.length; col++) {
+            if (assign[row][col] == 0) {
+                Pair p = new Pair(row, col);
+                solution.add(p);
+                
+                findSolutionCore(row + 1, solution);
+                solution.remove(p);
+            }
+        }
+    }
+    
+    private HashSet<Pair> findOptimalSolution() {
+        HashSet<Pair> optimalSolution = null;
+        int optimalCost = Integer.MAX_VALUE;
+        for (HashSet<Pair> solution : possibleSolutions) {
+            int cost = computeSolutionCost(solution);
+            if (cost < optimalCost) {
+                optimalCost = cost;
+                optimalSolution = solution;
+            }
+        }
+        return optimalSolution;
+    }
+    
+    private int computeSolutionCost(HashSet<Pair> solution) {
+        int cost = 0;
+        for (Pair p : solution) {
+            cost += matrix[p.row][p.col];
+        }
+        return cost;
+    }
+    
+    private void findSolution() {
+        HashSet<Pair> solution = new HashSet<>();
+        
+        long start = System.currentTimeMillis();
+        
+        findSolutionCore(0, solution);
+        
+        long end = System.currentTimeMillis();
+        
+        HashSet<Pair> optimalSolution = findOptimalSolution();
+        if (optimalSolution == null) {
+            System.out.println("No solution found :(");
+            return;
+        }
+        
+        int solutionCost = computeSolutionCost(optimalSolution);
+        
+        System.out.printf("Kuhn Munkres found %d possible solutions.\n", possibleSolutions.size());
+        
+        System.out.printf("Optimal solution found in %d ms:\nCost: %d\n", end -start, solutionCost);
+        for (Pair p : optimalSolution) {
+            System.out.println(p);
+        }
+               
+    }
 
     public void runKuhnMunkresAlgorithm(int[][] matrix) {
-    
-        int[][] assign = new int[matrix.length][matrix.length];
+        
+        this.matrix = matrix;
+        this.assign = new int[matrix.length][matrix.length];
+        this.possibleSolutions = new ArrayList<>();
         
         for (int i = 0; i < assign.length; i++) {
             copyTo(assign[i], matrix[i]);
@@ -42,14 +134,20 @@ public class KM {
             }
         }
         
-        if (isAssignmentValid(assign)) {
-            printSolution(assign, matrix);
-            return;
+        // Step two
+        for (int i = 0; i < assign.length; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < assign.length; j++) {
+                min = Math.min(min, matrix[j][i]);
+            }
+            
+            for (int j = 0; j < assign.length; j++) {
+                assign[j][i] = Math.max(0, assign[j][i] - min);
+            }       
         }
         
-        // Step two
-        
-        // Step three    
+        // Step three
+        findSolution();
     }
 
     public static void main(String args[]) {
